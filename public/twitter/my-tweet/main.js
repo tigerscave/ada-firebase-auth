c1b45d2da556ecf9d1fac991d70637830142f1f5
cdra0624 <t.adaniya@adawarp.com>
2019-05-16 18:12:00 +0900

    feat(myTweet): implement onSnapshot listener

--- public/twitter/my-tweet/main.js
+++ public/twitter/my-tweet/main.js
@@ -41,8 +41,55 @@ const fetchLatestPost = () => {
   })
 };
 
+const displayTweets = docs => {
+  const tweetsList = document.getElementById("tweets");
+  while (tweetsList.firstChild) {
+    tweetsList.removeChild(tweetsList.firstChild);
+  }
+  docs.forEach(d => {
+    const tweet = d.data();
+    const listNode = document.createElement("LI");
+    const textNode = document.createTextNode(tweet.content);
+    listNode.appendChild(textNode);
+
+    const imgListNode = document.createElement("LI");
+    const imgNode = document.createElement("IMG");
+    imgNode.src = tweet.imgURL ? tweet.imgURL : "";
+    imgNode.width = "200";
+    imgListNode.appendChild(imgNode);
+
+    tweetsList.appendChild(listNode);
+    tweetsList.appendChild(imgListNode);
+  })
+}
+
+const myTweetListener = (user) => {
+  const userId = user.uid;
+  console.log(userId)
+  db.collection("tweets")
+  .where("userId", "==", userId)
+  .orderBy("createdAt", "desc")
+  .onSnapshot((doc) => {
+    console.log(doc)
+    console.log(doc.docs)
+    displayTweets(doc.docs)
+  })
+}
+
+const checkUserAuth = () => {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (!user) {
+      window.location.replace("../login");
+    } else {
+      myTweetListener(user);
+    }
+  });
+};
+
 const main = () => {
+  checkUserAuth();
   db = firebase.firestore();
+
   const latestPostButton = document.getElementById('latestPostButton');
   latestPostButton.addEventListener('click', fetchLatestPost);
 };
