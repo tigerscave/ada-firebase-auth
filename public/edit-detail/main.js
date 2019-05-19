a5a09d77b117aa3d30a739431225ae09800da7ad
cdra0624 <t.adaniya@adawarp.com>
2019-05-19 19:04:47 +0900

    feat(user-detail): implement birthDay input

--- public/edit-detail/main.js
+++ public/edit-detail/main.js
@@ -5,9 +5,15 @@ let db = null;
 const checkUserAuth = () => {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      db.collection('users').doc(user.uid).get().then(doc => {
+      db.collection('users')
+      .doc(user.uid)
+      .get()
+      .then(doc => {
         console.log(doc.data());
-        const { biography } = doc.data();
+        const { biography, birthDay } = doc.data();
+        console.log(birthDay.toDate())
+        const formattedDate = dayjs(birthDay.toDate()).format('YYYY-MM-DD')
+        document.getElementById('birthDay').value = formattedDate;
         document.getElementById('biography').value = biography;
       });
     } else {
@@ -17,11 +23,15 @@ const checkUserAuth = () => {
 };
 
 const updateBio = () => {
+  const date = firebase.firestore.Timestamp;
   const bioInput = document.getElementById('biography');
+  const birthDayDate = new Date(document.getElementById('birthDay').value);
+  const birthDayTimeStamp = firebase.firestore.Timestamp.fromDate(birthDayDate);
   if(bioInput.value.length) {
     const user = firebase.auth().currentUser;
     db.collection('users').doc(user.uid).set({
-      biography: bioInput.value
+      biography: bioInput.value,
+      birthDay: birthDayTimeStamp
     }).then(() => {
       alert("success to update biography");
       window.location.href = "../user-detail"
--- public/user-detail/main.js
+++ public/user-detail/main.js
@@ -6,9 +6,17 @@ const checkUserAuth = () => {
   firebase.auth().onAuthStateChanged((user) => {
     document.getElementById('userName').innerHTML = user.displayName;
     if (user) {
-      db.collection('users').doc(user.uid).get().then(doc => {
+      db.collection('users')
+      .doc(user.uid)
+      .get()
+      .then(doc => {
         console.log(doc.data());
-        const { biography } = doc.data();
+        const { biography, birthDay } = doc.data();
+        console.log(birthDay)
+        console.log(birthDay.toDate())
+        const birthDayDate = birthDay.toDate();
+        const formattedBirthDay = dayjs(birthDayDate).format("YYYY-MM-DD");
+        document.getElementById('birthDay').innerHTML = formattedBirthDay;
         document.getElementById('biography').innerHTML = biography;
       });
     } else {
