a06ce3cd2e803be33180a211556f607632883a24
cdra0624 <t.adaniya@adawarp.com>
2019-05-19 18:27:41 +0900

    fear(myTweets): display createdAt

--- public/twitter/my-tweet/main.js
+++ public/twitter/my-tweet/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 let db = null;
+let firstVisible = null;
+let lastVisible = null;
+const OFFSET = 3;
 
 const displayLatestTweet = data => {
   const latestListEl = document.getElementById("latestTweet");
@@ -47,6 +50,8 @@ const displayTweets = docs => {
     tweetsList.removeChild(tweetsList.firstChild);
   }
   docs.forEach(d => {
+    console.log("data: ", d)
+    console.log("d.createdAt: ", d.data().createdAt.toDate())
     const tweet = d.data();
     const listNode = document.createElement("LI");
     const textNode = document.createTextNode(tweet.content);
@@ -58,8 +63,16 @@ const displayTweets = docs => {
     imgNode.width = "200";
     imgListNode.appendChild(imgNode);
 
+    const createTimeListNode = document.createElement("LI");
+    const date = d.data().createdAt.toDate();
+    const formattedDate =
+      date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + String(date.getMinutes()).padStart(2, '0');
+    const timeTextNode = document.createTextNode(`createdAt: ${formattedDate}`);
+    createTimeListNode.appendChild(timeTextNode);
+
     tweetsList.appendChild(listNode);
     tweetsList.appendChild(imgListNode);
+    tweetsList.appendChild(createTimeListNode);
   })
 }
 
@@ -69,10 +82,12 @@ const myTweetListener = (user) => {
   db.collection("tweets")
   .where("userId", "==", userId)
   .orderBy("createdAt", "desc")
-  .onSnapshot((doc) => {
-    console.log(doc)
-    console.log(doc.docs)
-    displayTweets(doc.docs)
+  .limit(OFFSET)
+  .onSnapshot((documentSnapshots) => {
+    console.log(documentSnapshots)
+    console.log(documentSnapshots.docs)
+    lastVisible = documentSnapshots.docs[documentSnapshots.docs.length - 1];
+    displayTweets(documentSnapshots.docs)
   })
 }
 
@@ -86,12 +101,52 @@ const checkUserAuth = () => {
   });
 };
 
+const paginate = (isForward) => {
+  const userId = firebase.auth().currentUser.uid;
+  if (isForward) {
+    db.collection("tweets")
+    .where("userId", "==", userId)
+    .orderBy("createdAt", "desc")
+    .startAfter(lastVisible)
+    .limit(OFFSET)
+    .get()
+    .then((documentSnapshots) => {
+      console.log(documentSnapshots)
+      console.log(documentSnapshots.docs)
+      firstVisible = documentSnapshots.docs[0];
+      lastVisible = documentSnapshots.docs[documentSnapshots.docs.length - 1];
+      displayTweets(documentSnapshots.docs)
+    })
+  } else {
+    console.log(firstVisible)
+    db.collection("tweets")
+    .where("userId", "==", userId)
+    .orderBy("createdAt", "desc")
+    .endBefore(lastVisible)
+    .limit(OFFSET)
+    .get()
+    .then((documentSnapshots) => {
+      console.log(documentSnapshots)
+      console.log(documentSnapshots.docs)
+      firstVisible = documentSnapshots.docs[0];
+      lastVisible = documentSnapshots.docs[documentSnapshots.docs.length - 1];
+      displayTweets(documentSnapshots.docs)
+    })
+  }
+}
+
 const main = () => {
   checkUserAuth();
   db = firebase.firestore();
 
   const latestPostButton = document.getElementById('latestPostButton');
   latestPostButton.addEventListener('click', fetchLatestPost);
+
+  const nextButton = document.getElementById('next');
+  nextButton.addEventListener('click', () => paginate(true));
+
+  const backButton = document.getElementById('back');
+  backButton.addEventListener('click', () => paginate(false));
 };
 
 window.addEventListener('DOMContentLoaded', main);
