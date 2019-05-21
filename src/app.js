cdc9cbea5f5ead47e2eb9ed3249c75b0684bbdcf
cdra0624 <t.adaniya@adawarp.com>
2019-05-21 17:55:45 +0900

    feat(Routing): add react-router-dom with welcome, login, sign-up pages

--- src/app.js
+++ src/app.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './app.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import WelcomePage from './pages/welcome';
+import SignUpPage from './pages/sign-up';
+import LoginPage from './pages/login';
+
+const Hello = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Firebase sample using React-Redux / Vanilla JS
+      </p>
+      <Link className="App-link" to="/welcome">
+        <p>React-Redux Sample</p>
+      </Link>
+      <a
+        className="App-link"
+        href="../vanilla-js-sample"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Vanilla JS Sample
+      </a>
+    </header>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="../vanilla-js-sample"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vanilla-js-sample
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={Hello} />
+      <Route path="/welcome" exact component={WelcomePage} />
+      <Route path="/sign-up" exact component={SignUpPage} />
+      <Route path="/login" exact component={LoginPage} />
+    </Router>
   );
 }
 
