da6b79ee53d6c941d614d7c4c7b4f11a947630b7
rhmt <rezaeihazara@gmail.com>
2019-05-23 10:42:38 +0900

    feat(sign-up): implement createUser function

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,9 +1,61 @@
 import React from "react";
+import firebase from "firebase/app";
 
-const SignUpPage = () => (
-  <div>
-    <h1>Sign Up</h1>
-  </div>
-);
+class SignUpPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+
+    this.onEmailChanged = e => {
+      this.setState({
+        email: e.target.value
+      });
+    };
+
+    this.onPasswordChanged = e => {
+      this.setState({
+        password: e.target.value
+      });
+    };
+
+    this.onSignUpUser = (email, password) => {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(() => {
+          alert("User sign up successfully");
+        })
+        .catch(() => {
+          alert("User Sign Up Failed");
+        });
+    };
+  }
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <input
+          placeholder="email"
+          onChange={this.onEmailChanged}
+          value={email}
+        />
+        <input
+          placeholder="password"
+          onChange={this.onPasswordChanged}
+          value={password}
+        />
+
+        <button onClick={() => this.onSignUpUser(email, password)}>
+          Sign up
+        </button>
+      </div>
+    );
+  }
+}
 
 export default SignUpPage;
