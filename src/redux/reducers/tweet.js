248ed7bccd009cdcaba7be836c95189c24626967
rhmt <rezaeihazara@gmail.com>
2019-05-30 17:22:35 +0900

    feat: fetch and display my tweets

--- src/redux/reducers/tweet.js
+++ src/redux/reducers/tweet.js
@@ -3,8 +3,19 @@ import { createAction } from "redux-actions";
 export const CREATE_TWEET = "CREATE_TWEET";
 export const createTweet = createAction(CREATE_TWEET);
 
+export const DISPLAY_TWEETS = "DISPLAY_TWEETS";
+export const displayTweets = createAction(DISPLAY_TWEETS);
+
+export const MY_TWEET_LISTENER = "MY_TWEET_LISTENER";
+export const myTweetListener = createAction(MY_TWEET_LISTENER);
+
+export const ON_SNAPSHOT_MY_TWEET = "ON_SNAPSHOT_MY_TWEET";
+export const onSnapshotMyTweet = createAction(ON_SNAPSHOT_MY_TWEET);
+
 const INITIAL_STATE = {
-  isLoading: false
+  isLoading: false,
+  latestTweet: null,
+  myTweets: []
 };
 
 const reducer = (state = INITIAL_STATE, action) => {
@@ -13,6 +24,30 @@ const reducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         isLoading: true
+      };
+    }
+
+    case DISPLAY_TWEETS: {
+      return {
+        ...state
+      };
+    }
+
+    case MY_TWEET_LISTENER: {
+      return {
+        ...state
+      };
+    }
+
+    case ON_SNAPSHOT_MY_TWEET: {
+      const docs = action.payload;
+      console.log("ON_SNAPSHOT_MY_TWEET", docs);
+      const myTweets = docs.map(doc => {
+        return doc.data();
+      });
+      return {
+        ...state,
+        myTweets
       };
     }
 
