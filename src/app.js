b6df5dff757bbc5d2d97c832267092b763db0d41
cdra0624 <t.adaniya@adawarp.com>
2019-06-01 17:25:05 +0900

    fix(app.js): add Hello component to show first view

--- src/app.js
+++ src/app.js
@@ -12,30 +12,30 @@ import TwitterTop from "./pages/twitter";
 import PostTweetPage from "./pages/twitter/post-tweet";
 import MyTweetPage from "./pages/twitter/my-tweet";
 
-// const Hello = () => (
-//   <div className="App">
-//     <header className="App-header">
-//       <img src={logo} className="App-logo" alt="logo" />
-//       <p>Firebase sample using React-Redux / Vanilla JS</p>
-//       <Link className="App-link" to="/welcome">
-//         <p>React-Redux Sample</p>
-//       </Link>
-//       <a
-//         className="App-link"
-//         href="../vanilla-js-sample"
-//         target="_blank"
-//         rel="noopener noreferrer"
-//       >
-//         Vanilla JS Sample
-//       </a>
-//     </header>
-//   </div>
-// );
+const Hello = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Firebase sample using React-Redux / Vanilla JS</p>
+      <Link className="App-link" to="/welcome">
+        <p>React-Redux Sample</p>
+      </Link>
+      <a
+        className="App-link"
+        href="../vanilla-js-sample"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Vanilla JS Sample
+      </a>
+    </header>
+  </div>
+);
 
 function App() {
   return (
     <Router history={history}>
-      {/*<Route path="/" exact component={Hello} />*/}
+      <Route path="/" exact component={Hello} />
       <Route path="/welcome" component={WelcomePage} />
       <Route path="/sign-up" component={SignUpPage} />
       <Route path="/login" component={LoginPage} />
